fcc471e51c7a4ab5acdbf258a2a876d468d9805a
Olaf Becker <olaf.becker@clio.com>
2019-04-29 20:37:34 -0700

    added pasport

--- server/expressServer.js
+++ server/expressServer.js
@@ -2,7 +2,12 @@ const app = require('express')();
 var httpProxy = require('http-proxy');
 var apiProxy = httpProxy.createProxyServer();
 const LicenseServer = require('./licenseServer');
-const config = require('../config.json');
+const config = require('./config.json');
+const uuid = require('uuidv4');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 /*
 // use express to handle incomming request
@@ -23,11 +28,70 @@ const credentials = {
 };
 const oauth2 = require('simple-oauth2').create(credentials);
 
-app.get('/login/:user', (req, res) => {
-    console.log("login");
-    LicenseServer.getLicense(req.params.user);
-    res.redirect(`http://localhost:${config.expressPort}/todo`);
+const users = [
+    { id: '2f24vvg', email: 'test@test.com', password: 'password' }
+]
+
+// configure passport.js to use the local strategy
+passport.use(new LocalStrategy(
+    { usernameField: 'email' },
+    (email, password, done) => {
+        console.log('Inside local strategy callback')
+        // here is where you make a call to the database
+        // to find the user based on their username or email address
+        // for now, we'll just pretend we found that it was users[0]
+        const user = users[0]
+        if (email === user.email && password === user.password) {
+            console.log('Local strategy returned true')
+            return done(null, user)
+        }
+    }
+));
+
+// tell passport how to serialize the user
+passport.serializeUser((user, done) => {
+    console.log('Inside serializeUser callback. User id is save to the session file store here')
+    done(null, user.id);
 });
+
+// add & configure middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(session({
+    genid: (req) => {
+        console.log('Inside the session middleware: ', req.sessionID)
+        return uuid() // use UUIDs for session IDs
+    },
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// create the login get and post routes
+app.get('/login', (req, res) => {
+    console.log('Inside GET /login callback function')
+    console.log(req.sessionID)
+    LicenseServer.getLicense(req.params.user);
+    res.redirect(`http://localhost:${config.expressPort}/login`);
+})
+
+app.post('/login', (req, res, next) => {
+    console.log('Inside POST /login callback')
+    passport.authenticate('local', (err, user, info) => {
+        console.log('Inside passport.authenticate() callback');
+        console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`)
+        console.log(`req.user: ${JSON.stringify(req.user)}`)
+        req.login(user, (err) => {
+            console.log('Inside req.login() callback')
+            console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`)
+            console.log(`req.user: ${JSON.stringify(req.user)}`)
+            return res.send('You were authenticated & logged in!\n');
+        })
+    })(req, res, next);
+})
+
 
 app.get('/oauth/response', async (req, res) => {
     console.log("oauth/response: ", req.query.code);
@@ -38,15 +102,16 @@ app.get('/oauth/response', async (req, res) => {
         grant_type: "authorization_code",
         redirect_uri: config.redirectUri,
     };
-    // Save the access token
+
+    // TODO: Save the access token for a specific user
     try {
         const result = await oauth2.authorizationCode.getToken(tokenConfig)
         console.log("result:", result);
-        const accessToken = oauth2.accessToken.create(result);
+        accessToken = oauth2.accessToken.create(result);
         console.log("accessToken:", accessToken);
 
         // redirect to the application home page
-        res.redirect(`http://localhost:${config.expressPort}/login/user1`);
+        res.redirect(`http://localhost:${config.expressPort}/login`);
 
     } catch (error) {
         console.log('Access Token Error', error);
@@ -61,17 +126,29 @@ app.get('/oauth', (req, res) => {
     });
     res.redirect(authorizationUri);
 });
+
 app.get('/api/v4/*', (req, res) => {
     console.log("redirecting to clio API");
+    req.headers.Authorization = `Bearer ${accessToken}`;
     apiProxy.web(req, res, { target: config.apiHost });
+})
+
+app.get('/authrequired', (req, res) => {
+    console.log('Inside GET /authrequired callback')
+    console.log(`User authenticated? ${req.isAuthenticated()}`)
+    if (req.isAuthenticated()) {
+        res.send('you hit the authentication endpoint\n')
+    } else {
+        res.redirect('/')
+    }
 })
 
 app.get('/*', (req, res) => {
     console.log("redirecting to app", `http://localhost:${config.webpackPort}` + req.originalUrl);
+    console.log("Session ID: ", req.sessionID);
     apiProxy.web(req, res, { target: `http://localhost:${config.webpackPort}` });
 })
 
 app.listen(config.expressPort, () => {
     console.log(`Listening on localhost:${config.expressPort}`)
 });
-
